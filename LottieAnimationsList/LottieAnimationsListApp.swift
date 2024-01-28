b6a494f0c7786d15daa5f869dec13ca1e9cbe349
Russell Gordon <rgordon@lcs.on.ca>
2024-01-28 17:46:24 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Russell Gordon on 2024-01-28.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
